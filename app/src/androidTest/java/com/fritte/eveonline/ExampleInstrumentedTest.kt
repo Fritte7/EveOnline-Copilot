8600388e87a90a7f40e3fd836e1fd9969b46c8ed
Fritte7 <hlaurent14@gmail.com>
2026-01-22 20:42:59 +0000

    Add the prefix for debug test

--- app/src/androidTest/java/com/fritte/eveonline/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fritte/eveonline/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fritte.eveonline", appContext.packageName)
+        assertEquals("com.fritte.eveonline.debug", appContext.packageName)
     }
 }
